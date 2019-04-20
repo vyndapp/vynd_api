4d09ad062b967a931f6a98870fe5404138d4f95c
omarkhaledabdo <omarkhaledabdo@gmail.com>
2019-04-20 17:54:39 +0200

    Move repeated url_to_img to test_utils

--- vynd_api/test/test_utils.py
+++ vynd_api/test/test_utils.py
@@ -1,9 +1,17 @@
+from io import BytesIO
 import os
 from PIL import Image
 import numpy as np
+import requests
 
 def get_img_from_filename(img_name: str):
     cur_dir = os.path.dirname(__file__)
     impath = os.path.join(cur_dir, 'resources/' + img_name)
     img = Image.open(impath)
     return np.asarray(img)
+
+def url_to_img(url: str):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    return np.asarray(img)
+

f5201aaae4a594c47a638744459f85625cdf2bb2
omarkhaledabdo <omarkhaledabdo@gmail.com>
2019-05-31 15:34:06 +0200

    change host

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ def create_app(config_filename):
 if __name__ == "__main__":
     app = create_app("config")
 # host = '0.0.0.0' makes the server visible across the network (Extremly Visible Server)
-    app.run(debug=True, threaded=True)
+    app.run(debug=True, host='0.0.0.0', threaded=True)

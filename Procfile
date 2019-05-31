d8953e6e586f2644c71b7c59e3a70ce23dd056e7
omarkhaledabdo <omarkhaledabdo@gmail.com>
2019-05-31 14:43:19 +0200

    add procfile

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python setup.py
+web: python run.py
--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@
 from vynd_api.models.download_vggface_model import download_vggface_model
 
 if __name__ == "__main__":
+    print ('Started setup.py\n\n\n\n')
     download_vggface_model()

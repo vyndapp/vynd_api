06ede42d395bf817f9f4525604b0086993da2f17
omarkhaledabdo <omarkhaledabdo@gmail.com>
2019-05-31 16:57:08 +0200

    debug post/put

--- vynd_api/api/resources/add_new_video.py
+++ vynd_api/api/resources/add_new_video.py
@@ -8,6 +8,6 @@ class AddNewVideo(Resource):
 
     video_collection = VideoCollection(CLIENT.vynd_db_test.video_collection)
 
-    def post(self):
+    def put(self):
         # video_id = self.video_collection.insert_new_video()
         return jsonify(video_id="123")

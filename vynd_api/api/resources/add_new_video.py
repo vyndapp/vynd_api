f9771325421b0c46843df9579fe63fc472c91288
yahya.alaa <yahyaalaamassoud@gmail.com>
2019-06-10 18:39:07 +0200

    handle errors in add_new_video resource

--- vynd_api/api/resources/add_new_video.py
+++ vynd_api/api/resources/add_new_video.py
@@ -1,6 +1,7 @@
 
 from flask_restful import Resource, request
-from flask import jsonify
+from flask import jsonify, make_response
+from flask_api import status
 
 from .. import CLIENT, VideoCollection
 
@@ -9,6 +10,12 @@ class AddNewVideo(Resource):
     video_collection = VideoCollection()
 
     def post(self):
-        video_id = self.video_collection.insert_new_video()
-        request.stream.read()
-        return jsonify(video_id=video_id)
+        if request.get_json() and request.get_json().get('extension'):
+            extension = request.get_json().get('extension')
+            video_id = self.video_collection.insert_new_video(video_extension=extension)
+            request.stream.read()
+            return make_response(jsonify(video_id=video_id), 
+                                 status.HTTP_200_OK)
+        else:
+            return make_response(jsonify(error="Send video's extension along with new video addition's request"),
+                                 status.HTTP_400_BAD_REQUEST)

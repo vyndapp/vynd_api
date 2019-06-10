54d5360b4d2d981dbb24f4939584269dfda42e68
yahya.alaa <yahyaalaamassoud@gmail.com>
2019-06-10 18:38:52 +0200

    update name update in face_updater

--- vynd_api/api/resources/face_updater.py
+++ vynd_api/api/resources/face_updater.py
@@ -1,6 +1,7 @@
 
 from flask_restful import Resource, request
-from flask import jsonify
+from flask import jsonify, make_response
+from flask_api import status
 
 from .. import CLIENT, FaceCollection
 
@@ -12,9 +13,13 @@ class FaceUpdater(Resource):
     face_collection = FaceCollection()
     
     def post(self):
-        face_id = request.form['face_id']
-        name = request.form['name']
+        if request.get_json() and request.get_json().get('face_id') and request.get_json().get('name'):
+            face_id = request.get_json().get('face_id')
+            name = request.get_json().get('name')
 
-        update_result = self.face_collection.update_name(face_id, name)
+            update_result = self.face_collection.update_name(face_id, name)
 
-        return jsonify(success=update_result)
+            return jsonify(success=update_result)
+        else:
+            return make_response(jsonify(error="Send video's extension along with new video addition's request"),
+                                 status.HTTP_400_BAD_REQUEST)

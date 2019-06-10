77b7663bb47b6f1111cbcb242509877f11021e67
yahya.alaa <yahyaalaamassoud@gmail.com>
2019-06-10 18:40:26 +0200

    update __init__ in api directory

--- vynd_api/api/__init__.py
+++ vynd_api/api/__init__.py
@@ -2,6 +2,7 @@
 from ..data import CLIENT
 from ..data.face_collection import FaceCollection
 from ..data.video_collection import VideoCollection
+from ..data.keyframe_collection import KeyFrameCollection
 from ..utils.image_utils import base64_to_rgb
 from ..videoprocessing.video_processor import VideoProcessor
 from ..videoprocessing.video_processing_results import VideoProcessingResult

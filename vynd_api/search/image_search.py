39fa437b917d64f3f36afa778507e4345abda2ea
yahya.alaa <yahyaalaamassoud@gmail.com>
2019-06-11 23:34:30 +0200

    add face_collection to image_search

--- vynd_api/search/image_search.py
+++ vynd_api/search/image_search.py
@@ -5,8 +5,9 @@ from ..facerecognition.faceembedding.vggface_embedder import VGGFaceEmbedder
 from ..facerecognition.facematching.image_faces_matcher import ImageFacesMatcher
 from ..facerecognition.facematching.face_match_status import FaceMatchStatus
 from ..data.face_collection import FaceCollection
+from ..data import CLIENT
 
-def search_by_image(face_collection, image: np.ndarray):
+def search_by_image(image: np.ndarray, face_collection=CLIENT.vynd_db.face_collection):
     detection_results = YOLOv3Detector().detect(keyframe=KeyFrame(keyframe_image=image))
     embeddings = VGGFaceEmbedder().faces_to_embeddings(detection_results)
     matches = ImageFacesMatcher(face_collection=face_collection).match_faces(embeddings)
